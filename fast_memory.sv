4e1e27a5b1694cd2363a25af18c80611a2c8de50
DeepanCoder <42707342+DeepanCoder@users.noreply.github.com>
2024-03-06 22:41:21 +0530

    Create fast_memory.sv
    Creating fast memory module first trail

---
+++ fast_memory.sv
@@ -0,0 +1,32 @@
+///////////////////////////////
+//          Fast Memory      //
+//    Author: Deepan Kumar   //
+///////////////////////////////
+module Memory(input logic clk,rstn,
+    input logic [31:0] address,
+    input logic [63:0] data_in,
+    input logic write_enable,
+    input logic read_enable,
+    output logic [63:0] data_out
+);
+
+    // Define memory array with appropriate depth
+    logic [63:0] mem [0:(1<<14)-1]; // 14-bit address for cache memory
+
+    // Read and write operations
+  always_ff @(posedge clk or !rstn) begin
+    if (!rstn) data_out=0;
+        else if (read_enable) begin
+            data_out = mem[address];
+        end
+    else data_out = 0;
+    end
+
+  always_ff @(posedge clk or !rstn) begin
+    if(!rstn) mem[address] = 0;
+        if (write_enable) begin
+            mem[address] = data_in;
+        end
+    end
+
+endmodule
